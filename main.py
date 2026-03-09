8f6820e814800e05a0f50ab1980ff19f34a193cc
Dylan Baldwin <dbaldwin@mtech.edu>
2026-03-09 12:36:18 -0600

    Finished Assignment

--- main.py
+++ main.py
@@ -9,12 +9,58 @@
 ;understand the importance the consequences of plagiarism.
 """
 
-def point_orientation_testing(p1:tuple[int], p2:tuple[int], p3:tuple[int])->str:
-    pass
+"""
+Source: https://www.geeksforgeeks.org/dsa/check-if-two-given-line-segments-intersect/
 
-def add_positive_integers(a: int, b: int) -> int:
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise ValueError("Inputs must be integers")
-    if a < 0 or b < 0:
-        raise ValueError("Inputs must be positive")
-    return a + b
+Usage: structure for intersection section
+
+"""
+def get_orientation(p:tuple[float], q:tuple[float], r:tuple[float])->str:
+    result = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+    if result > 0:
+        return "Clockwise"
+    elif result < 0:
+        return "Counter Clockwise"
+    else:
+        return "Linear"
+    
+def on_segment(p:tuple[float], q:tuple[float], r:tuple[float]) -> float:
+    result = ((r[0] - p[0]) * (q[1] - p[1])) - ((r[0] - p[0]) * (q[0] - p[0]))
+
+    if result == 0:
+        return True
+    else:
+        return False
+    
+
+def do_segments_intersect(seg1, seg2):
+    p1, p2 = seg1
+    p3, p4 = seg2
+
+    o1 = get_orientation(p1, p2, p3)
+    o2 = get_orientation(p1, p2, p4)
+    o3 = get_orientation(p3, p4, p1)
+    o4 = get_orientation(p3, p4, p2)
+
+    if o1 != o2 and o3 != o4: 
+        return True
+    if o1 == "Linear" and on_segment(p1, p3, p2):
+      return True
+    if o2 == "Linear" and on_segment(p1, p4, p2):
+      return True
+    if o3 == "Linear" and on_segment(p3, p1, p4):
+      return True
+    if o4 == "Linear" and on_segment(p3, p2, p4):
+        return True
+
+    return False
+
+
+if __name__ == "__main__":
+    p1 = (1.0, 1.0)
+    p2 = (4.0, 4.0)
+    p3 = (2.0, 4.0)
+    p4 = (4.0, 2.0)
+    seg1 = (p1, p2)
+    seg2 = (p3, p4)
+    print(do_segments_intersect(seg1, seg2)) # Output: True
--- tests.py
+++ tests.py
@@ -1,17 +1,44 @@
+"""
+;Dylan Baldwin
+;CSCI 322 Spring 2025
+;Programming Assignment #class17
+;I acknowledge that I have worked on this assignment independently, except where
+;explicitly noted and referenced. Any collaboration or use of external resources
+;has been properly cited. I am fully aware of the consequences of academic
+;dishonesty and agree to abide by the university's academic integrity policy. I
+;understand the importance the consequences of plagiarism.
+"""
+
 import unittest
-from main import add_positive_integers
+from main import do_segments_intersect
 
-class TestMathFunctions(unittest.TestCase):
-    def test_sum_correct(self):
-        self.assertEqual(add_positive_integers(5, 10), 15)
+class TestSegmentIntersection(unittest.TestCase):
+    def test_default(self):
+        p1 = (1.0, 1.0)
+        p2 = (4.0, 4.0)
+        p3 = (2.0, 4.0)
+        p4 = (4.0, 2.0)
+        seg1 = (p1, p2)
+        seg2 = (p3, p4)
+        self.assertEqual(do_segments_intersect(seg1, seg2),True)
 
-    def test_negative_input(self):
-        with self.assertRaises(ValueError):
-            add_positive_integers(-1, 5)
-
-    def test_wrong_type(self):
-        with self.assertRaises(ValueError):
-            add_positive_integers("5", 5)
+    def test_do_not_intersect(self):
+        p1 = (0.0, 0.0)
+        p2 = (0.0, 1.0)
+        p3 = (1.0, 0.0)
+        p4 = (1.0, 1.0)
+        seg1 = (p1, p2)
+        seg2 = (p3, p4)
+        self.assertEqual(do_segments_intersect(seg1, seg2),False)
+    
+    def test_overlaping(self):
+        p1 = (0.0, 0.0)
+        p2 = (0.0, 2.0)
+        p3 = (0.0, 1.0)
+        p4 = (0.0, 3.0)
+        seg1 = (p1, p2)
+        seg2 = (p3, p4)
+        self.assertEqual(do_segments_intersect(seg1,seg2), True)   
 
 if __name__ == "__main__":
     unittest.main()
